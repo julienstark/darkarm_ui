7868c1b3849be007d9691f2c68b33c615f02fdf7
Julien Thieffry <julien.thieffry@eamslab.com>
2018-11-21 14:02:19 +0900

    roboarm_views: adding the JogJointScreen class

--- roboarm_views.py
+++ roboarm_views.py
@@ -75,3 +75,102 @@ class InfoScreen(rau.Screen):
         tk.Message(right_canvas,
                    text="Access the command panels with top tabs.",
                    width=500).pack(fill=tk.X)
+
+
+class JogJointScreen(rau.Screen):
+    """Screen displaying the Jog joint options and controls for
+    the Robot Arm.
+
+    Attributes:
+        header_canvas: Canvas containing the screen title.
+        header_message: Message containing the screen title text.
+        servobar: Checkbar instance representing the servo tab bar.
+        rot_tree: Tree instance representing the rotation measure array.
+        content_canvas: Canvas acting as the main screen container.
+        inc_canvas: Canvas containing the increment objects.
+        jog_inc: Message containing the Jog Increment text.
+        inc_entry_canvas: Canvas containing the entry text and arrow for
+        the increment.
+        arrowleft: Left arrow button.
+        entry_field = Entry widget for increment.
+        arrowright: Right arrow button.
+        grip_canvas: Canvas containing the grip button widgets.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Default JogJointScreen builder."""
+
+        rau.Screen.__init__(self, *args, **kwargs)
+
+        header_canvas = tk.Frame(self, relief=tk.RAISED, borderwidth=1)
+        header_canvas.pack(fill=tk.BOTH, pady=25)
+
+        header_message = tk.Message(header_canvas,
+                                    text="Jog Joint",
+                                    width=200)
+        header_message.config(anchor='n',
+                              font=("times", 24, "bold"),
+                              pady=5)
+        header_message.pack(side=tk.TOP, expand=True)
+
+        servobar = rau.Checkbar(self,
+                                [
+                                 'Servo 1',
+                                 'Servo 2',
+                                 'Servo 3',
+                                 'Servo 4',
+                                 'Servo 5',
+                                 'Servo 6',
+                                ])
+        servobar.pack(fill=tk.X)
+        servobar.config(relief=tk.GROOVE, bd=2)
+
+        rot_tree = ttk.Treeview(self)
+        rot_tree["columns"]=("value")
+        rot_tree.column("value", width=100)
+        rot_tree.heading("value", text="Measure")
+
+        rot_tree.insert("", 0, text="Angle 6", values=("180"))
+        rot_tree.insert("", 0, text="Angle 5", values=("180"))
+        rot_tree.insert("", 0, text="Angle 4", values=("180"))
+        rot_tree.insert("", 0, text="Angle 3", values=("180"))
+        rot_tree.insert("", 0, text="Angle 2", values=("180"))
+        rot_tree.insert("", 0, text="Angle 1", values=("180"))
+
+        rot_tree.pack(side=tk.LEFT, expand=False)
+
+        content_canvas = tk.Frame(self)
+        content_canvas.pack(fill=tk.X, expand=True)
+
+        inc_canvas = tk.Frame(content_canvas)
+        inc_canvas.pack(fill=tk.X, expand=True)
+
+        jog_inc = tk.Message(inc_canvas,
+                             text="Jog Increment",
+                             width=300)
+        jog_inc.pack(fill=tk.BOTH, expand=True)
+        
+        inc_entry_canvas = tk.Frame(inc_canvas)
+        inc_entry_canvas.pack()
+
+        arrowleft = tk.Button(inc_entry_canvas, text="<")
+        arrowleft.pack(side=tk.LEFT)
+
+        entry_field = tk.Entry(inc_entry_canvas,
+                               justify=tk.CENTER)
+        entry_field.pack(side=tk.LEFT)
+
+        arrowright = tk.Button(inc_entry_canvas, text=">")
+        arrowright.pack(side=tk.LEFT)
+
+        grip_canvas = tk.Frame(content_canvas)
+        grip_canvas.pack(fill=tk.BOTH, expand=True)
+
+        tk.Button(grip_canvas,
+                  text="Open Grip").pack(anchor="s",
+                                         side=tk.LEFT,
+                                         expand=True)
+        tk.Button(grip_canvas,
+                  text="Close Grip").pack(anchor="s",
+                                          side=tk.LEFT,
+                                          expand=True)
